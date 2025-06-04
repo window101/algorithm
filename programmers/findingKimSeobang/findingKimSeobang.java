4e1532c85ca26438c57c37efe72cedbd66606138
hwajun <hwajun15@naver.com>
2025-06-04 22:13:00 +0900

    [프로그래머스]서울에서 김서방 찾기

---
+++ programmers/findingKimSeobang/findingKimSeobang.java
@@ -0,0 +1,13 @@
+package programmers.findingKimSeobang;
+
+public class findingKimSeobang {
+    public String solution(String[] seoul) {
+        String answer = "";
+        for(int i = 0; i<seoul.length; i++) {
+            if(seoul[i].equals("Kim")) {
+                answer = "김서방은 " + i +"에 있다";
+            }
+        }
+        return answer;
+    }
+}
