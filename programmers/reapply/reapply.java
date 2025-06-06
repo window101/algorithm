958cd0431219bfdc0f342295bc2da48f67df60b5
hwajun <hwajun15@naver.com>
2025-06-06 13:10:57 +0900

    [프로그래머스]덧칠하기

---
+++ programmers/reapply/reapply.java
@@ -0,0 +1,30 @@
+package programmers.reapply;
+
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        
+        int answer = 0;
+        int indx = 0;
+        while(indx < section.length) {
+            int s = section[indx];
+            int e = section[indx] + m - 1;
+            answer++;
+            while(indx < section.length && section[indx] <= e) {
+                indx++;
+            }
+            
+        }
+        return answer;
+    }
+}
+public class reapply {
+    public static void main(String[] args) {
+
+        int n = 8;
+        int m = 4;
+        int [] section = new int[] {2,3,6};
+        Solution sol = new Solution();
+        System.out.println(sol.solution(n,m,section));
+
+    }
+}
