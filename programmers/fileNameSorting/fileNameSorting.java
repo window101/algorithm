1583abf8c0838a5a689f30cfc87ba075c480fa7a
hwajun <hwajun15@naver.com>
2025-06-07 17:33:12 +0900

    [프로그래머스] 파일명 정렬

---
+++ programmers/fileNameSorting/fileNameSorting.java
@@ -0,0 +1,69 @@
+package programmers.fileNameSorting;
+
+import java.util.Arrays;
+
+
+class Solution {
+
+    static class FileSort {
+        public int indx;
+        public String number = "";
+        public int realNum;
+        public String head = "";
+        public String tail = "";
+        public String str;
+        FileSort(String str, int indx) {
+            this.str = str;
+            this.indx = indx;
+            parse();
+        }
+        public void parse() {
+            
+            char [] arr = str.toLowerCase().toCharArray();
+            int i = 0;
+            while(i < arr.length && !Character.isDigit(arr[i])) {
+                head += arr[i];
+                i++;
+            }
+            while(i < arr.length && Character.isDigit(arr[i]) && number.length() <= 5) {
+                number += arr[i];
+                i++;
+            }
+            while(i < arr.length) {
+                tail += arr[i];
+                i++;
+            }
+            realNum = Integer.parseInt(number);
+        }
+
+
+    }
+    public String[] solution(String[] files) throws Exception{
+        
+        FileSort[] filesort = new FileSort[files.length];
+        for(int i = 0; i<files.length; i++) {
+            filesort[i] = new FileSort(files[i], i);
+        }
+        Arrays.sort(filesort, (a,b) -> {
+            int headComp = a.head.compareTo(b.head);
+            if(headComp != 0) return headComp;
+            int numCompt = Integer.compare(a.realNum, b.realNum);
+            return numCompt;
+        });
+        String[] answer = new String[files.length];
+        for(int i = 0; i<files.length; i++) {
+            answer[i] = filesort[i].str;
+        }
+
+
+        return answer;
+    }
+}
+public class fileNameSorting {
+    public static void main(String[] args) throws Exception {
+        String [] files = new String[] {"img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"};
+
+        Solution sol = new Solution();
+        System.out.println(Arrays.toString(sol.solution(files)));
+    }
+}
