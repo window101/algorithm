974c344b0e4f822fefbe660a768436a087bf9388
hwajun <hwajun15@naver.com>
2025-11-22 16:14:46 +0900

    [백준] 상근이의 여행

---
+++ baekjoon/problem6/problem6.java
@@ -0,0 +1,18 @@
+package baekjoon.problem6;
+
+import java.util.*;
+public class problem6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for(int i = 0; i<T; i++) {
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+            for(int j = 0; j<M; j++) {
+                int s = sc.nextInt();
+                int e = sc.nextInt();
+            }
+            System.out.println(N-1);
+        }
+    }
+}
